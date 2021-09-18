f0186cfe02b52d677803141119276eea00b94220
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-18 18:59:32 +0200

    Create tasks.json andf TasksAPI.js

---
+++ src/components/TasksAPI.js
@@ -0,0 +1,44 @@
+class TasksAPI {
+    constructor() {
+        this.url = 'http://localhost:3000/tasks';
+    }
+
+    loadData() {
+        return this._fetch()
+    }
+
+    addData(data) {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        return this._fetch(options)
+    }
+
+    updateData(id, data) {
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        return this._fetch(options,`/${id}`)
+    }
+
+    _fetch(options, additionalPath='') {
+        const url = this.url + additionalPath;
+        return fetch(url, options)
+            .then(resp =>{
+                if(resp.ok){
+                    return resp.json();
+                }
+                return Promise.reject(resp)
+            });
+    }
+}
+
+export default TasksAPI;
--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,18 +1,87 @@
 import React from 'react';
+import TasksAPI from './TasksAPI';
+const tasksDB = new TasksAPI(); 
 
 class TasksManager extends React.Component {
     state = {
-        tasks: [],
+        tasks: []
     }
-
-    onClick = () => {
-        const { tasks } = this.state;
-        console.log( tasks)
-    }
-
+    
     render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
+        return (
+            <>
+                <h1>TasksManager</h1>
+                <section onSubmit = {this.addTask}>
+                <form>
+                    <input name ="title" onChange={this.changeInput}/>
+                    <input name="time" onChange={this.changeInput}></input>
+                    <input type ="submit"/>
+                </form>
+                </section>
+                <section>
+                    <ul>{this.renderTaskList()}</ul>
+                </section>
+            </>
+        )
     }
+
+    renderTaskList() {
+        return (this.state.tasks.map(item => {
+            const {title, time, id, isRemoved}= item;
+            if(!isRemoved) {
+                return (
+                    <li data-id={id}> 
+                        <header>
+                            <h2>{title}</h2>
+                            <p>Czas:{time}sekund</p>
+                        </header>
+                        <footer>
+                            <button onClick ={this.runTask}>start</button>
+                            <button>zakończone</button>
+                            <button>usuń</button>
+                        </footer>
+                    </li>
+                )
+            }
+        }))
+    }
+
+
+
+    componentDidMount (){
+        tasksDB.loadData()
+         .then(res => {
+            return res.map(item => { console.log(item)
+           this.setData(item)}
+                )
+         })
+         // .then(res => console.log(res))
+     }
+     setData = e => {
+         this.setState({tasks: data});
+     }
+ 
+ 
+     onClick = () => {
+         const { tasks } = this.state;
+         console.log( tasks)
+     }
+
+    addTask = e => {
+        e.preventDefault();
+        const {title, time} = e.target.elements
+        const data = {
+            title: title.value,
+            time: time.value,
+        }
+        console.log(data, typeof data)
+        tasksDB.addData(data)
+    }
+
+    changeInput =e => {
+        // console.log(e.target.value)
+    }
+
 }
 
 export default TasksManager;
